ab100b59aea51135e6fc839ef1c2b145261ac6e0
Luis Eugenio Candelaria Azpilcueta <luiseugenio.candelaria@gmail.com>
2020-06-16 09:13:24 -0500

    models

--- Server coding challenge/models/moviedex-model.js
+++ Server coding challenge/models/moviedex-model.js
@@ -1,10 +1,48 @@
 const mongoose = require( 'mongoose' );
 const uuid = require( 'uuid' );
 
-/* 
-    Your code goes here 
-*/
+let movieSchema = mongoose.Schema({
+    movie_ID : {
+        type : String,
+        unique : true,
+        required : true
+    },
+    movie_title : {
+        type : String,
+    },
+    movie_year : {
+        type : Number
+    },
+    movie_rating : {
+        type : Number
+    }
+})
+
+let moviesCollection = new mongoose.Connection( 'movies', movieSchema );
+
+const Movies = {
+    addNewMovie: function( movie ) {
+        return moviesCollection
+                .create( movie )
+                .then( response => {
+                    return response;
+                })
+                .catch( err => {
+                    return err;
+                })
+    },
+    getAllMovies: function() {
+        return moviesCollection
+                .find()
+                .then( response => {
+                    return response;
+                })
+                .catch( err => {
+                    return err;
+                })
+    }
+}
 
 module.exports = {
-    
+    Movies
 };
