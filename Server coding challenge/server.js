f1e25e1d00f895178bf4ec018c220c6edb8b7598
Luis Eugenio Candelaria Azpilcueta <luiseugenio.candelaria@gmail.com>
2020-06-16 09:13:51 -0500

    server fix

--- Server coding challenge/server.js
+++ Server coding challenge/server.js
@@ -4,6 +4,8 @@ const mongoose = require( 'mongoose' );
 const jsonParser = bodyParser.json();
 const { DATABASE_URL, PORT } = require( './config' );
 const cors = require( './middleware/cors' );
+const uuid = require( 'uuid' );
+const {Movies} = require('./models/moviedex-model');
 
 const app = express();
 
@@ -12,6 +14,52 @@ app.use( cors );
 /* 
     Your code goes here 
 */
+
+app.get('/api/movies/', (req, res) => {
+    Movies
+        .getAllMovies()
+            .then( response => {
+                if( response != null ) {
+                    return res.status( 200 ).json( response );
+                }
+                else {
+                    res.statusMessage = "No movies found in the moviedex.";
+                    return res.status( 404 ).end();
+                }
+            })
+            .catch( err => {
+                console.log( "error" );
+            })
+})
+
+app.post('/api/add-movie/', jsonParser, (req, res) => {
+    let movieTitle = req.body.movie_title;
+    let movieYear = req.body.movie_year;
+    let movieRating = req.body.movie_rating;
+    let movieID = uuid.v4();
+
+    if( !movieTitle || !movieRating || !movieYear ) {
+        res.statusMessage = "You need to send all movie fields to add the movie to the movie list";
+        return res.status( 403 ).end();
+    }
+
+    let data = {
+        movie_ID : movieID,
+        movie_title : movieTitle,
+        movie_rating : movieRating,
+        movie_year : movieYear
+    }
+
+    Movies
+        .addNewMovie( data )
+            .then( response => {
+                return res.status( 201 ).json( response );
+            })
+            .catch( err => {
+                console.log( "error" );
+            })
+
+})
 
 app.listen( PORT, () => {
     console.log( "This server is running on port 8080" );
